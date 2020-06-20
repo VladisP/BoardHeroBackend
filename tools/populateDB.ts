264595d96fddadac751ad87ad38dc01abf49a317
Vladislav Pichugin <vlad19041999@yandex.ru>
2020-06-20 20:49:28 +0500

    [db] simple query from node client

--- tools/populateDB.ts
+++ tools/populateDB.ts
@@ -1,5 +1,19 @@
-function populate(s: string): void {
-    console.log(s);
+import { Client, QueryConfig } from 'pg';
+
+async function performQuery(): Promise<void> {
+    const client: Client = new Client();
+    await client.connect();
+
+    const query: QueryConfig = {
+        text: 'INSERT INTO board_games(board_game_id, game_name, description, image_url, small_image_url) VALUES($1, $2, $3, $4, $5)',
+        values: ['123e4567-e89b-12d3-a456-426655440000', 'kek', 'lol', 'url', 'url2']
+    };
+
+    try {
+        await client.query(query);
+    } finally {
+        await client.end();
+    }
 }
 
-populate('eee');
+performQuery().catch(console.error);
