ff1cc0ba021ddb8cb5ff4984f5f282ee78af701d
Vladislav Pichugin <vlad19041999@yandex.ru>
2020-06-20 14:52:34 +0500

    add first sql files and init db script

---
+++ sql/drop_all.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS game_category_int;
+DROP TABLE IF EXISTS game_mechanic_int;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS mechanics;
+DROP TABLE IF EXISTS board_games;
