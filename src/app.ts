fe90774b49d386c0e393a8169387a4e327a81b26
Vladislav Pichugin <vlad19041999@yandex.ru>
2020-06-23 11:39:29 +0500

    [games] add 404 response

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { ServerConfig } from './config/config';
 import { router } from './router/apiRouter';
+import { errorResponse, StatusCode } from './common/responseSender';
 
 const app = express();
 const { host, port } = ServerConfig.get();
 
 app.use(bodyParser.json());
 app.use('/api/', router);
+app.use('*', (req, res) => errorResponse(req, res, StatusCode.NOT_FOUND, new Error('Not Found')));
 
 app.listen(port, host, () => console.log(`Server listens http://${host}:${port}`));
