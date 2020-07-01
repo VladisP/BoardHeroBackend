b59a6e599a23f83d3563b769be3251c473443f03
Vladislav Pichugin <vlad19041999@yandex.ru>
2020-07-02 01:32:08 +0500

    [master] add cors and fix cookie maxAge

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as session from 'express-session';
 import * as connectRedis from 'connect-redis';
+import * as cors from 'cors';
 import { ServerConfig } from './config/config';
 import { router } from './router/apiRouter';
 import { errorResponse, StatusCode } from './common/responseSender';
@@ -10,11 +11,12 @@ const app = express();
 const { host, port, sessionSecret, redisClient } = ServerConfig.get();
 const RedisStore = connectRedis(session);
 
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
 app.use(bodyParser.json());
 app.use(session({
     cookie: {
         httpOnly: true,
-        maxAge: 3600 * 24
+        maxAge: 3600 * 24 * 1000
     },
     resave: false,
     saveUninitialized: false,
